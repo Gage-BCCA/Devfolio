f887b3c73efde1ae3549c998652facb68487a312
SpencerHillhouse <shillhouse@basecampcodingacademy.org>
2025-01-13 16:26:10 -0600

    Started on registration page

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Account(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    location = models.CharField(max_length=50)
+    links = models.CharField(max_length=200)
+    password = models.CharField(max_length=50)
+    password2 = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group
+from .models import *
+from .form import *
+from portfolios.urls import *
+from portfolios.views import *
+from portfolios.templates.portfolios import *
 # Create your views here.
+
+def registerPage(request):
+    form = CreateAccount()
+    if request.method == "POST":
+        form = CreateAccount(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # group = Group.objects.get(name='accountholder')
+            # print("THIS IS WHAT YOU ARE LOOKING FOR", group)
+            # user.groups.add(group)
+            return redirect('index')
+    context = {'form':form}
+    return render(request, 'accounts/register.html', context)
