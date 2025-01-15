ce13ca3130a34cfd07f024ae3d71a8383c92c30c
JetRobertsBCCA <jroberts@basecampcodingacademy.org>
2025-01-14 18:35:39 -0600

    oops

--- portfolios/urls.py
+++ portfolios/urls.py
@@ -3,7 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('create-portfolio/', views.create_portfolio, name='create_portfolio'),
-    path('portfolio/', views.portfolio_detail, name='portfolio_detail'),
-    path('example/', views.example_portfolio, name="example"),
 ]
