c614c8ef68ebcc48970aabea570175886f5bc8de
JetRobertsBCCA <jroberts@basecampcodingacademy.org>
2025-01-14 20:06:09 -0600

    added views

--- portfolios/urls.py
+++ portfolios/urls.py
@@ -4,5 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('portfolio_detail', views.portfolio_details, name='portfolio_detail'),
+    path('portfolio/create/', views.portfolio_create, name='portfolio_create'),
+    path('project/create/', views.project_create, name='project_create'),
+    path('skill/create/', views.skill_create, name='skill_create'),
+    path('bio/create/', views.bio_create, name='bio_create'),
+    path('link/create/', views.link_create, name='link_create'),
 
 ]
--- portfolios/views.py
+++ portfolios/views.py
@@ -1,10 +1,53 @@
 from django.shortcuts import render, redirect
-
+from .forms import PortfolioForm, ProjectForm, SkillForm, BioForm, LinkForm
 # Create your views here.
 def index(request):
     return render(request, 'portfolios/index.html')
 
+def portfolio_create(request):
+    form = PortfolioForm()
+    if request.method == 'POST':
+        form = PortfolioForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    return render(request, 'portfolios/portfolio_form.html', {'form': form})
 
+def project_create(request):
+    form = ProjectForm()
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    return render(request, 'portfolios/project_form.html', {'form': form})
+
+def skill_create(request):
+    form = SkillForm()
+    if request.method == 'POST':
+        form = SkillForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    return render(request, 'portfolios/skill_form.html', {'form': form})
+
+def bio_create(request):
+    form = BioForm()
+    if request.method == 'POST':
+        form = BioForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    return render(request, 'portfolios/bio_form.html', {'form': form})
+
+def link_create(request):
+    form = LinkForm()
+    if request.method == 'POST':
+        form = LinkForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    return render(request, 'portfolios/link_form.html', {'form': form})
 
 
 
